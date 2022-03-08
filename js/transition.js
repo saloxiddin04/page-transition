08dcabd8361ea4f76257507900c85f226bbab79f
Saloxiddin701 <abduraxmon.69list.ru@gmail.com>
2022-03-08 15:40:50 +0500

    page

---
+++ js/transition.js
@@ -0,0 +1,22 @@
+import Highway from "@dogstudio/highway";
+import {TimelineLite} from 'gsap';
+
+class Fade extends Highway.Transition {
+    in({from, to, done}) {
+        const tl = new TimelineLite();
+        tl.fromTo(to, 0.5, {left: '-100%', top: '50%'}, {left: '0%'})
+            .fromTo(to, 0.5, {height: '2vh'}, {
+                height: '90vh', top: '10%', onComplete: function () {
+                    from.remove();
+                    done()
+                }
+            })
+            .fromTo(to.children[0], 2, {opacity: 0}, {opacity: 1})
+    }
+
+    out({from, done}) {
+        done()
+    }
+}
+
+export default Fade;
